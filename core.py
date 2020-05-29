d4369e133bb2c8e6f1c95b4d672dd80a3eb57175
Michael Roberts <michaelroberts@hackucf.org>
2020-05-29 18:37:48 -0400

    Fixed OsType

--- core.py
+++ core.py
@@ -1,11 +1,10 @@
-import vars
 import platform
 
 def init_globals():
     temp_platform_var = platform.linux_distribution()[0]
-    if temp_platform_var == "CentOS Linux"
-        g_OsType = Centos
-    if temp_platform_var == "Ubuntu"
-        g_OsType = Ubuntu
+    if temp_platform_var == "CentOS Linux":
+        OsType = Centos
+    if temp_platform_var == "Ubuntu":
+        OsType = Ubuntu
 
-print g_OsType
+print OsType
