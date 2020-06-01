8543fb4ec41ebafde1f5795dacb2e50581f07bbe
ChrisFischer-MTA <ChrisFischer-MTA@users.noreply.github.com>
2020-05-31 20:50:56 -0400

    Major Fixes
    Added a bunch of fixes for broken logic surrounding dictionaries. Fixed logic error with source/destination IP. Added logic for warnings, added logic to save old rules, added logic to create a baseline for scan. Fixed typo in TCP scanning. Added wrapper function in get IP to ensure either 127.0.0.1/127.0.0.1 exist in chain. Tested

--- firewall.py
+++ firewall.py
@@ -1,6 +1,6 @@
 # Christopher Fischer
 # 5/31/2020
-# Last Edit: 5/31/2020 9:30 AM
+# Last Edit: 5/31/2020 8:50 PM
 # snake case naming used for convention
 # Python 3
 
@@ -13,7 +13,7 @@ import socket
 # TODO: Migrate this to core.py
 # TODO: Look into if we should be returning IP opjects instead of strings with IPs
 # TODO: Test if we're getting IPv6. Test with multiple stuff.
-def get_IP():
+def get_IP_true():
     # Credit:
     # Stackoverflow - 8529181 & 3503879/
     ip_addresses = os.popen("hostname -I").read()
@@ -23,6 +23,14 @@ def get_IP():
     else:
         # Return a list of the singular IP Address
         return [ip_addresses]
+
+def get_IP():
+    results = get_IP_true()
+    if "127.0.0.1" not in results:
+        if "127.0.1.1" not in results:
+            results.append("127.0.0.1")
+    return results
+
 
 class firewall:
     def __init__(self, OSType):
@@ -34,19 +42,24 @@ class firewall:
         self.OSType = OSType
         self.firewallUsed = None
         for i in self.firewallList:
-            if(self.firewallList[i][OSType] == 1):
-                self.firewallUsed = self.firewallList[i]
+            if(i[OSType] == 1):
+                self.firewallUsed = i
+                break
 
         # TCP Test
-        def __ip_test(self, ip):
+    def ip_test(self, ip):
             results = []
-            for port in range(0, 65534):
+            for port in range(0, 32768):
                 test = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
                 result = test.connect_ex((ip, port))
-                results.append(1) if result == 1 else results.append(0)
+                if result is 0:
+                    # At some point, we can just use format strings but I'm lazy
+                    print("[WARN] Class (firewall) function (ip_test) message:  "+str(ip)+":"+str(port)+" is open!")
+                results.append(1) if result == 0 else results.append(0)
+            return results
 
         # UDP Test
-        def __ip_test_udp(self, ip):
+    def __ip_test_udp(self, ip):
             # Unfortunately, the implementation of UDP for Linux doesn't allow this.
             return -1
 
@@ -56,7 +69,7 @@ class firewall:
         ip_addresses = get_IP()
         for ip in ip_addresses:
             # Spray ports
-            ports = self.__ip_test(self, ip)
+            ports = self.ip_test(ip)
             results[ip] = ports
         # Returns a dictionary with {ip : listOfPorts} as convention
         return results
@@ -69,20 +82,18 @@ class firewall:
             # Can't find suitable candidate
             return -1001
         if self.firewallUsed['FirewallType'] is 1:
-            ports = self.__socket_test(self)
             # Let's start with a baseline
+            print("[INFO] Class (firewall) function (ip_test) message: Saving current IPTables rules to /etc/oldrules.ip")
+            os.system("iptables-save > /etc/oldrules.ip")
+            os.system("iptables -P INPUT ACCEPT")
             os.system("iptables -F")
             os.system("iptables -X")
-            os.system("iptables -P INPUT DROP")
-            os.system("iptables -P OUTPUT ALLOW")
+            # Changed to accept for unit testing
+            os.system("iptables -P OUTPUT ACCEPT")
             os.system("iptables -P FORWARD DROP")
+            ports = self.__socket_test()
+            os.system("iptables -P INPUT DROP")
             for ip in ports:
-                for i in range(0,65534):
-                    if ports[i] is 1:
-                        os.system("iptables -A INPUT -p tcp --dport "+i+" -s "+ip+ " -j ACCEPT")
-            return
-
-
-
-
-
+                for i in range(0,32767):
+                    if ports[ip][i] is 1:
+                        os.system("iptables -A INPUT -p tcp --dport "+str(i)+" -d "+str(ip)+ " -j ACCEPT")
